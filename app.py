37278a57d3594c0a5504239260c1b917f867c2e2
otnansirk <iam.otnansirk@gmail.com>
2024-10-31 20:21:41 +0700

    feat : gemini AI

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+
+load_dotenv()
+
+def genai_wrapper(promt):
+    genai.configure(api_key=os.environ["GPT_API_KEY"])
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(promt)
+    return response.text
 
 def reading(text):
     tts = gTTS(text=text, lang='id')
@@ -17,13 +30,14 @@ def voice_job():
         try:
             print("Recognizing")
             voice_to_text = recog.recognize_google(voice, language="id-ID").lower()
-            
             if(voice_to_text == 'terima kasih'):
                 reading("Sama sama, senang bisa membantu")
                 exit()
 
-            print(voice_to_text)
-            reading(voice_to_text)
+            voice_res = genai_wrapper(voice_to_text)
+
+            print('Processing...')
+            reading(voice_res)
 
 
         except sr.UnknownValueError:
